dbbfe02a30958e61cf68ca68aa93ab95b0daf208
tpltnt <tpltnt.github@dropcut.net>
2015-05-22 23:38:12 +0200

    sqr cheat

--- s2/e2.1.1.scm
+++ s2/e2.1.1.scm
@@ -2,6 +2,9 @@
 ;;(** 2) meh
 ;;(sqr 2) also not
 ;;... hmmm no explicit operator in scheme (scm)?
+;; cheating by looking two paragraphs ahead
+(define (sqr n) (* n n))
+(sqr 2)
 ;; sine of an angle
 (sin 1)
 ;; maximum of two number
